000b14a1033b8b8ab07777593c8ea8144c4f038c
K10051997 <134429184+K10051997@users.noreply.github.com>
2025-04-09 13:12:04 -0400

    Update streamlit.py

--- credit_eligibility_application-main/streamlit.py
+++ credit_eligibility_application-main/streamlit.py
@@ -18,7 +18,7 @@ based on various personal and financial characteristics.
 #    st.stop()
 
 # Load the pre-trained model
-rf_pickle = open("random_forest_credit.pickle", "rb")
+rf_pickle = open("credit_eligibility_application-main/random_forest_credit.pickle", "rb")
 rf_model = pickle.load(rf_pickle)
 rf_pickle.close()
 
